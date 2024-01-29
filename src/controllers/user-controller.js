1a20130f4b870342b302b1c3971c79025309e695
ShujaShah <shuja.ashraf11@gmail.com>
2024-01-29 20:02:17 +0530

    code refactored

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -1,20 +1,19 @@
 const { User, validateUser } = require('../models/entities/user-entity');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const AppError = require('../utilities/AppError');
 const catchAsync = require('../utilities/catchAsync');
 
 const createUser = catchAsync(async (req, res, next) => {
   const { error } = validateUser(req.body);
-  // if (error) return res.status(500).send(error.details[0].message);
-  if (error)
-    return next(
-      new AppError(error.details[0].message || 'something went wrong', 400)
-    );
+  if (error) {
+    return res.status(400).send('Invalid user data');
+  }
 
   let user = await User.findOne({ email: req.body.email });
 
   if (user) {
-    return next(new AppError('user already exists', 400));
+    return res.status(400).send('User already exists');
   }
 
   user = new User({
@@ -24,7 +23,7 @@ const createUser = catchAsync(async (req, res, next) => {
     role: req.body.role,
   });
 
-  //hash the password
+  // Hash the password
   const salt = await bcrypt.genSalt(10);
   user.password = await bcrypt.hash(user.password, salt);
 
@@ -46,13 +45,31 @@ const createUser = catchAsync(async (req, res, next) => {
   res.cookie('auth_token', auth_token, auth_cookie_options);
   res.cookie('refresh_token', refresh_token, refresh_cookie_options);
 
-  user = await user.save();
+  //user = await user.save();
+  const { activationCode, token } = createActivationToken(user);
   res.status(201).json({
-    succesfull: 'true',
-    user,
-    auth_token,
-    refresh_token,
+    user: user,
+    code: activationCode,
+    token: token,
   });
 });
+
+//Create 2FA and activation token
+const createActivationToken = (user) => {
+  const activationCode = Math.floor(1000 + Math.random() * 9000).toString(); // Generate a random four-digit code
+
+  // Create a JSON Web Token, token contains the payload with two properties: user and 4 digit activation code
+  const token = jwt.sign(
+    {
+      user,
+      activationCode,
+    },
+    process.env.JWTPrivateKey,
+    {
+      expiresIn: '10m',
+    }
+  );
+  return { token, activationCode };
+};
 
 module.exports = { createUser };
