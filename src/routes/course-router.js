68e6fde298d18306775e0c9594f9292570ac7963
ShujaShah <shuja.ashraf11@gmail.com>
2024-02-08 19:41:30 +0530

    added auth middlewares

--- src/routes/course-router.js
+++ src/routes/course-router.js
@@ -10,10 +10,10 @@ const isAuthenticated = require('../middlewares/auth');
 const isAdmin = require('../middlewares/admin');
 const courseRouter = express.Router();
 
-courseRouter.post('/create-course', isAuthenticated, uploadCourse);
-courseRouter.patch('/edit-course/:id', isAuthenticated, editCourse);
+courseRouter.post('/create-course', isAuthenticated, isAdmin, uploadCourse);
+courseRouter.patch('/edit-course/:id', isAuthenticated, isAdmin, editCourse);
 courseRouter.get('/', getAllCourses);
 courseRouter.get('/:id', getSingleCourse);
-courseRouter.delete('/:id', deleteCourse);
+courseRouter.delete('/:id', isAuthenticated, isAdmin, deleteCourse);
 
 module.exports = courseRouter;
