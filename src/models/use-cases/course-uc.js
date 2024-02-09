f3ebac01423807a598c1117861b928224bdd6a35
ShujaShah <shuja.ashraf11@gmail.com>
2024-02-09 14:59:11 +0530

    BugFix: Removed the video url while fetching courses

--- src/models/use-cases/course-uc.js
+++ src/models/use-cases/course-uc.js
@@ -24,7 +24,9 @@ const updateCourse = async (courseId, data, res) => {
 
 //Get All Courses
 const getCourses = async (res) => {
-  const courses = await Course.find();
+  const courses = await Course.find().select(
+    '-courseData.videoUrl -courseData.questions -courseData.links'
+  );
   if (!courses) return res.status(400).send('no courses in DB');
   const count = await Course.countDocuments();
   res.status(201).json({
@@ -36,7 +38,9 @@ const getCourses = async (res) => {
 
 //Get Single Course
 const getOneCourse = async (courseId, res) => {
-  const course = await Course.findById(courseId);
+  const course = await Course.findById(courseId).select(
+    '-courseData.videoUrl -courseData.questions -courseData.links'
+  );
   if (!course)
     return res.status(400).send('course with this id does not exist');
   res.status(201).json({
