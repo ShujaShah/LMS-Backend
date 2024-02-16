08c8c6a2cd4fa9bd8bdd00766a394836dbd8f513
ShujaShah <shuja.ashraf11@gmail.com>
2024-02-16 14:36:53 +0530

    created a notifications model

---
+++ src/models/entities/notifications-entity.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const notificationSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      default: 'unread',
+    },
+  },
+  { timestamps: true }
+);
+
+const Notification = mongoose.model('Notification', notificationSchema);
+
+module.exports = Notification;
